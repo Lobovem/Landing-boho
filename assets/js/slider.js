bd8b3c5e712e1e7d1647bdb31e646238da90bdde
Lobov Evgeniy <lobov.em@gmail.com>
2022-12-22 01:14:15 +0100

    Закончил слайдер.

---
+++ assets/js/slider.js
@@ -0,0 +1,55 @@
+const swiper = new Swiper('.swiper', {
+  spaceBetween: 20,
+
+  // Optional parameters
+  // direction: 'vertical',
+  // loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  //Количество слайдов на странице
+
+  // slidesPerView: 3,
+
+  //Адаптив
+
+  breakpoints: {
+    310: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    400: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+
+    },
+
+    768: {
+      slidesPerView: 4,
+    },
+
+    1000: {
+      slidesPerView: 5,
+    },
+
+    1300: {
+      slidesPerView: 6,
+    }
+  },
+
+  // Navigation arrows
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
+});
+
